35f2e4a315d23844bee4f3dc81f3600e3aca3f35
Zakaria <191528459+Zakaria-Engineer@users.noreply.github.com>
2025-05-02 18:59:36 +0100

    Add files via upload

---
+++ src/components/pages/Home/MealCard.js
@@ -0,0 +1,27 @@
+import { faUtensils } from '@fortawesome/free-solid-svg-icons'; 
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { Link } from 'react-router-dom';
+import './MealCard.css';
+import pages from '../../../utils/pages';
+
+const MealCard = ({ meal }) => {
+  return (
+    <article className="meal-card">
+      <div className="meal-card-image">
+        <img src={meal.image} alt={meal.name} />
+      </div>
+      <div className="meal-card-header">
+        <h3>{meal.name}</h3>
+        <span>{meal.price}</span>
+      </div>
+      <div className="meal-card-body-footer">
+        <p>{meal.description}</p>
+        <Link to={pages.get('orderOnline').path} className="order-link">
+        Get your meal delivered <FontAwesomeIcon icon={faUtensils} />
+        </Link>
+      </div>
+    </article>
+  );
+};
+
+export default MealCard;
